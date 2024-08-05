cd3c46cb5c17bbbaea93cbdf721f367d76511971
raps4g <raps4g@gh>
2024-08-05 18:51:56 -0300

    changed how to achieve acute vowels and n tilde

--- keyboards/crkbd/rev1/keymaps/raps4g-dvorak/keymap.c
+++ keyboards/crkbd/rev1/keymaps/raps4g-dvorak/keymap.c
@@ -28,64 +28,9 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 	NULL
 };
 
-enum custom_keycodes {
-    ES_AAC = SAFE_RANGE,
-    ES_EAC,
-    ES_IAC,
-    ES_OAC,
-    ES_UAC,
-    ES_AACUP,
-    ES_EACUP,
-    ES_IACUP,
-    ES_OACUP,
-    ES_UACUP,
-    ES_UDIAE,
-
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case ES_AAC:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_TAP(X_A)) : clear_keyboard();
-            break;
-        case ES_EAC:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_TAP(X_E)) : clear_keyboard();
-            break;
-        case ES_IAC:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_TAP(X_I)) : clear_keyboard();
-            break;
-        case ES_OAC:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_TAP(X_O)) : clear_keyboard();
-            break;
-        case ES_UAC:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_TAP(X_U)) : clear_keyboard();
-            break;
-        case ES_AACUP:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_DOWN(X_LSFT) SS_TAP(X_A)) : clear_keyboard();
-            break;
-        case ES_EACUP:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_DOWN(X_LSFT) SS_TAP(X_E)) : clear_keyboard();
-            break;
-        case ES_IACUP:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_DOWN(X_LSFT) SS_TAP(X_I)) : clear_keyboard();
-            break;
-        case ES_OACUP:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_DOWN(X_LSFT) SS_TAP(X_O)) : clear_keyboard();
-            break;
-        case ES_UACUP:
-            (record->event.pressed) ? SEND_STRING(SS_TAP(X_LBRC) SS_DOWN(X_LSFT) SS_TAP(X_U)) : clear_keyboard();
-            break;
-        case ES_UDIAE:
-            (record->event.pressed) ? SEND_STRING(SS_DOWN(X_LSFT) SS_TAP(X_LBRC) SS_UP(X_LSFT) SS_TAP(X_U)) : clear_keyboard();
-            break;
-    }
-    return true;
-}
-
 #define _BASE 0
 #define _NS 1
 #define _FN 2
-#define _LAT 3
 #define _MCR 4
 
 #define MOD_O LSFT_T(ES_O)
@@ -102,6 +47,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define MOD_8 RCTL_T(ES_8)
 #define MOD_9 RSFT_T(ES_9)
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint16_t lat_state;
+    if (keycode == ES_ACUT && !record->event.pressed) {
+        lat_state = 1;
+    }
+    if (lat_state && !record->event.pressed) {
+        switch (keycode) {
+            case ES_A:
+            case MOD_O:
+            case MOD_E:
+            case MOD_U:
+            case ES_I:
+                lat_state = 0;
+                break;
+            case MOD_N:
+                tap_code16(KC_BSPC);
+                tap_code16(ES_NTIL);
+                lat_state = 0;
+                return false;
+        }
+    }
+    return true;
+}
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -109,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       ES_SLSH,    ES_A,   MOD_O,   MOD_E,   MOD_U,    ES_I,                         ES_D,   MOD_H,   MOD_T,   MOD_N,    ES_S, ES_MINS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       ES_EQL, ES_SCLN,    ES_Q,    ES_J,    ES_K,    ES_X,                         ES_B,    ES_M,    ES_W,    ES_V,    ES_Z,OSL(_LAT),
+       ES_EQL, ES_SCLN,    ES_Q,    ES_J,    ES_K,    ES_X,                         ES_B,    ES_M,    ES_W,    ES_V,    ES_Z, ES_ACUT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                 TT(_MCR), TT(_NS),   LGUI_T(KC_SPC),     KC_ENT, TT(_FN),  KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -140,18 +110,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
-    [_LAT] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,ES_AACUP,ES_OACUP,ES_EACUP,ES_UACUP,ES_IACUP,                         ES_F,    ES_G,    ES_C,    ES_R,    ES_L,TG(_LAT),
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      ES_SLSH,  ES_AAC,  ES_OAC,  ES_EAC,  ES_UAC,  ES_IAC,                         ES_D,    ES_H,    ES_T, ES_NTIL,    ES_S, ES_DIAE,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       ES_EQL, ES_SCLN,    ES_Q,    ES_J,ES_UDIAE,    ES_X,                         ES_B,    ES_M,    ES_W,    ES_V,   ES_Z,OSL(_LAT),
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                TT(_MCR), TT(_NS),   LGUI_T(KC_SPC),     KC_ENT, TT(_FN),  KC_RALT
-                                      //`--------------------------'  `--------------------------'
-
-  ),
     [_MCR] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,                      XXXXXXX, KC_MS_BTN1, KC_MS_BTN3, KC_MS_BTN2, XXXXXXX, QK_BOOT,
@@ -173,7 +131,6 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 bool oled_task_user() {
     oled_set_brightness(80);
     if (!is_keyboard_master()) {
-        //render_lisa();
         oled_set_cursor(0,4);
         render_matecito();
         return false;
@@ -188,9 +145,6 @@ bool oled_task_user() {
             break;
         case _FN:
             render_layer_fn();
-            break;
-        case _LAT:
-            render_layer_lat();
             break;
         case _MCR:
             render_layer_macros();
